3683424e9a8656a1b41871e8a73d392704e85c59
Eric McKinleyi <eric.mckinley@10xbanking.com>
2019-12-11 11:51:21 +0000

    P4 non root mvn

--- pipeline/scripts/java_build_project.sh
+++ pipeline/scripts/java_build_project.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Java build via mvn docker image"
-docker run --rm --name maven-project-type -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.6.3-jdk-8-slim mvn clean package
+docker run --rm --name maven-project-type -u 1000 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.6.3-jdk-8-slim mvn -Duser.home=/var/maven clean package
