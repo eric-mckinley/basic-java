7e553741b089a475abdb510f26ad113f2155111e
Eric McKinleyi <eric.mckinley@10xbanking.com>
2019-12-11 12:59:28 +0000

    P8

--- src/main/java/fts/BasicTool.java
+++ src/main/java/fts/BasicTool.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BasicTool {
 
     public static void main(String[] args) {
-        System.out.println("Tool works.");
+        System.out.println("Tool started.");
         String lastSortedItem = Arrays.stream(args).sorted(String::compareToIgnoreCase).findFirst().orElse("Nothing");
         System.out.println("Java8 sorted value: " + lastSortedItem);
     }
