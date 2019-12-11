c26c796d2aae54743424738142a654549967d998
Eric McKinleyi <eric.mckinley@10xbanking.com>
2019-12-11 09:52:41 +0000

    P2

--- pipeline/scripts/java_build_project.sh
+++ pipeline/scripts/java_build_project.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-
-docker run -it --rm --name maven-project-type -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.6.3-jdk-8-slim mvn clean package
+#!/bin/bash
+echo "Java build via mvn docker image"
+docker run --rm --name maven-project-type -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.6.3-jdk-8-slim mvn clean package
